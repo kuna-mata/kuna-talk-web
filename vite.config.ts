24f6881c675facc0ed33fe8970ed772d7ab5cf5c
ha-ccoon <codeminhasohn@gmail.com>
2024-10-30 18:01:12 +0900

    chore: modify vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from "vite";
-import { VitePWA as pwa } from "vite-plugin-pwa";
-import manifest from "./manifest.json";
-import react from "@vitejs/plugin-react";
+import { defineConfig, UserConfig, InlineConfig } from 'vite';
+import { VitePWA as pwa } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react';
+
+import manifest from './manifest.json';
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: { globals: true, assetsInclude: ['__test__/*.spec.ts?(x)'] },
   plugins: [
     react(),
     pwa({
-      strategies: "injectManifest",
-      srcDir: "",
-      filename: "service-worker.js",
+      strategies: 'injectManifest',
+      srcDir: '',
+      filename: 'service-worker.js',
       manifest,
     }),
   ],
-});
+} as VitestConfigExport);
